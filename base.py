fb8db1a79e195eb596ac063065fff00b9b444879
WeiFoo <wfu@ncsu.edu>
2015-11-17 00:31:56 -0500

    refactor

--- base.py
+++ base.py
@@ -14,25 +14,6 @@ class Options:
   def __init__(i, **d):
     i.__dict__.update(d)
 
-# Settings = Options(de=Options(np=20,
-#                               repeats=1000,
-#                               f=0.75,
-#                               cr=0.85,
-#                               threshold=0.0001,
-#                               limit=Options(infoPrune = 1, 
-#                                             dTreeMin = 1,
-#                                             threshold = 1,
-#                                             whereMinSize  = 1,    # min leaf size, percentage
-#                                             whereDepthMin= 2,      # no pruning till this depth
-#                                             wehreDepthMax= 20,     # max tree depth
-#                                             whereWriggle = 0.2,    # min difference of 'better'
-#                                             wherePrune   = True,   # pruning enabled?
-#                                            ),
-#                               life=5,
-#                               k = 0,
-#                               run = 20
-#                               ))
-
 
 Settings = Options(de=Options(np=10,
                               repeats=1000,
